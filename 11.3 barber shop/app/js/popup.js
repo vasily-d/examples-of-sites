c58a90f3cd85d8866c1e3291945c9a2999e02156
vasily-d <den771981@mail.ru>
2017-09-15 09:20:16 +0300

    work in header and some fix:12 nerds index

--- 11.3 barber shop/app/js/popup.js
+++ 11.3 barber shop/app/js/popup.js
@@ -1,60 +1,65 @@
-var elementLogin = document.querySelector(".login");
-var elementPopup = document.querySelector(".popup");
-var elementClosePopup = document.querySelector(".popup .close");
-var elementLoginInPopup = elementPopup.querySelector("[name=login]"); 
-var elementPasswordInPopup = elementPopup.querySelector("[name=password]"); 
-var form = elementPopup.querySelector("form");
-var storage = localStorage.getItem("login"); //null ?
+window.onload = function(){
+	var elementLogin = document.querySelector(".login");
+	var elementPopup = document.querySelector(".modal-content");
+	var elementClosePopup = document.querySelector(".modal-content .close");
+	var elementLoginInPopup = elementPopup.querySelector("[name=login]"); 
+	var elementPasswordInPopup = elementPopup.querySelector("[name=password]"); 
+	var form = elementPopup.querySelector("form");
+	var storage = localStorage.getItem("login"); //null ?
+	var modalOverlay = document.querySelector('.modal-overlay');
 
-// событие на клик по логину в header
-elementLogin.addEventListener("click", function(event){
-	event.preventDefault();
+	// событие на клик по логину в header
+	elementLogin.addEventListener("click", function(event){
+		event.preventDefault();
 
-	//elementClosePopup.style.display = "block";
-	elementPopup.classList.add("show");
+		//elementClosePopup.style.display = "block";
+		elementPopup.classList.add("show");
+		modalOverlay.style.display = 'block';
 
-	// focus на login
-	elementLoginInPopup.focus();
-
-	if(storage){
-		elementLoginInPopup.value = storage;
-		elementPasswordInPopup.focus();	
-	}
-	else{
 		// focus на login
 		elementLoginInPopup.focus();
-	}
-});
 
-// событие на закрытие popup
-elementClosePopup.addEventListener("click", function(event){
-	event.preventDefault();
-	elementPopup.classList.remove("show");
-	elementPopup.classList.remove("modal-error");
-});
-
-// событие на отправку формы
-form.addEventListener("submit", function(event){
-	var valueLogin = elementLoginInPopup.value;
-	var valuePassword = elementPasswordInPopup.value;
-	
-	if(!valueLogin || !valuePassword){
-		event.preventDefault();
-		elementPopup.classList.add("modal-error");
-		console.log("error in login or/and password");
-	}
-	else{
-		localStorage.setItem("login", valueLogin);
-		console.log("ok");
-	}
-});
-
-window.addEventListener("keydown", function(event){
-	if (event.keyCode == 27){
-		if (elementPopup.classList.contains("show")){
-			elementPopup.classList.remove("show");
-			elementPopup.classList.remove("modal-error");
-
+		if(storage){
+			elementLoginInPopup.value = storage;
+			elementPasswordInPopup.focus();	
 		}
-	}
-});
+		else{
+			// focus на login
+			elementLoginInPopup.focus();
+		}
+	});
+
+	// событие на закрытие popup
+	elementClosePopup.addEventListener("click", function(event){
+		event.preventDefault();
+		elementPopup.classList.remove("show");
+		elementPopup.classList.remove('modal-error');
+		modalOverlay.style.display = 'none';
+	});
+
+	// событие на отправку формы
+	form.addEventListener("submit", function(event){
+		var valueLogin = elementLoginInPopup.value;
+		var valuePassword = elementPasswordInPopup.value;
+		
+		if(!valueLogin || !valuePassword){
+			event.preventDefault();
+			elementPopup.classList.add("modal-error");
+			console.log("error in login or/and password");
+		}
+		else{
+			localStorage.setItem("login", valueLogin);
+			console.log("ok");
+		}
+	});
+
+	window.addEventListener("keydown", function(event){
+		if (event.keyCode == 27){
+			if (elementPopup.classList.contains("show")){
+				elementPopup.classList.remove("show");
+				elementPopup.classList.remove("modal-error");
+				modalOverlay.style.display = 'none';
+			}
+		}
+	});
+};
--- 12.1 nerds catalog/gulpfile.js
+++ 12.1 nerds catalog/gulpfile.js
@@ -1,10 +1,10 @@
 //npm i browser-sync --save-dev
 //npm i gulp-less --save-dev
+
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    browserSync = require('browser-sync');
-
-var notify = require("gulp-notify");
+    browserSync = require('browser-sync'),
+    notify = require("gulp-notify");
 
 gulp.task('browserSync', function() {
   browserSync({
@@ -16,7 +16,7 @@ gulp.task('browserSync', function() {
 });
 
 gulp.task('less', function(){
-  return gulp.src('app/css/*.less')
+  return gulp.src('app/less/*.less')
   .pipe(less())
   .on('error', notify.onError(function(err){
     return {
@@ -30,11 +30,12 @@ gulp.task('less', function(){
 
 
 gulp.task('default', ['browserSync', 'less'], function (){
-  gulp.watch('app/css/**/*.less', ['less']);
-  
-  //gulp.watch('app/less/**/*', browserSync.reload); 
-  //gulp.watch('app/css/**/*.css', browserSync.reload); 
+  gulp.watch('app/less/**/*.less', ['less']);
   
   // Обновляем браузер при любых изменениях в HTML или JS
+  gulp.watch('app/js/*.js', browserSync.reload);
   gulp.watch('app/*.html', browserSync.reload);
+
+  //gulp.watch('app/less/**/*', browserSync.reload); 
+  //gulp.watch('app/css/**/*.css', browserSync.reload); 
 });
