15c07acf8f7f6243836aefbffe182d6baeef3bb1
vasily-d <den771981@mail.ru>
2017-09-25 15:04:50 +0300

    add effect:13 hosting

--- 13 hosting/app/js/custom.js
+++ 13 hosting/app/js/custom.js
@@ -183,5 +183,19 @@ $(function(){
 		});
 	})();
 
+	// wow js
+	new WOW().init();
+
+	// wow = new WOW(
+	// 	{
+	// 		boxClass:     'wow',      // default
+	// 		animateClass: 'animated', // default
+	// 		offset:       0,          // default
+	// 		mobile:       true,       // default
+	// 		live:         true        // default
+	// 	}
+	// )
+	// wow.init();
+
 
 });
