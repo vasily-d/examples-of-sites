a18193c042de80ea8eaf5d740a62899363a739bd
vasily-d <den771981@mail.ru>
2017-09-25 11:50:33 +0300

    markup sedon:14 sedon

---
+++ 14 sedon/gulpfile.js
@@ -0,0 +1,41 @@
+//npm i browser-sync --save-dev
+//npm i gulp-less --save-dev
+
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    browserSync = require('browser-sync'),
+    notify = require("gulp-notify");
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'	//папка нашего корневика c сайтом, иначе '' если gulp там же
+    },
+    notify: false
+  });
+});
+
+gulp.task('less', function(){
+  return gulp.src('app/less/*.less')
+  .pipe(less())
+  .on('error', notify.onError(function(err){
+    return {
+      title: 'Styles',
+      message: err.message
+    };
+  }))
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.reload({stream: true}))
+});
+
+
+gulp.task('default', ['browserSync', 'less'], function (){
+  gulp.watch('app/less/**/*.less', ['less']);
+  
+  // Обновляем браузер при любых изменениях в HTML или JS
+  gulp.watch('app/js/*.js', browserSync.reload);
+  gulp.watch('app/*.html', browserSync.reload);
+
+  //gulp.watch('app/less/**/*', browserSync.reload); 
+  //gulp.watch('app/css/**/*.css', browserSync.reload); 
+});
