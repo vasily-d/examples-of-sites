7e7cba68c4f1532ba29d744f48e95ea7b3a88139
vasily-d <den771981@mail.ru>
2017-10-01 21:11:15 +0300

    add js to nerds and sedon:12 nerds, 14 sedon

--- 14 sedon index-hotels/app/js/hotels-icheck.js
+++ 14 sedon index-hotels/app/js/hotels-icheck.js
@@ -3,4 +3,17 @@ $(function() {
 		checkboxClass: 'icheckbox_polaris',
 		radioClass: 'iradio_polaris'
 	});
+
+	$("#range").ionRangeSlider({
+		// min: 0,
+		// max: 10000,
+		// from: 1000,
+		// to: 9000,
+		type: 'double',
+		hide_min_max: true,
+		hide_from_to: true
+		// prefix: "$",
+		// grid: true,
+		// grid_num: 10
+	});
 })
