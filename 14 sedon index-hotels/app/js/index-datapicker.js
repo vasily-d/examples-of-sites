20281cc7a82d70dfc5d389b44d335c30219a16dc
vasily-d <den771981@mail.ru>
2017-10-08 17:11:58 +0300

    markup - 12 nerds, add popup js - 14 sedon

--- 14 sedon index-hotels/app/js/index-datapicker.js
+++ 14 sedon index-hotels/app/js/index-datapicker.js
@@ -4,4 +4,14 @@ $(function() {
 	});
 	
 	new WOW().init();
+
+	$('.sentence h2 a').on('click', function(e) {
+		e.preventDefault();
+
+		$('.popup').fadeIn(200);
+	});
+
+	$('.popup h3').on('click', function(e) {
+		$('.popup').fadeOut(200);
+	});
 })
