11bf9ecbf3ab1f38ca55d3846c1a4812f6fe5c81
vasily-d <den771981@mail.ru>
2017-10-31 11:25:03 +0300

    update gulpfile.js, package.json

--- 14 icrowdme adaptive+js effect+simple cms/gulpfile.js
+++ 14 icrowdme adaptive+js effect+simple cms/gulpfile.js
@@ -1,10 +1,19 @@
 //npm i browser-sync --save-dev
 //npm i gulp-less --save-dev
 
+//npm install --save-dev del
+//npm install gulp-cssnano --save-dev
+//npm install run-sequence --save-dev
+//npm install --save-dev gulp-imagemin
+
 var gulp = require('gulp'),
     less = require('gulp-less'),
     browserSync = require('browser-sync'),
-    notify = require("gulp-notify");
+    notify = require('gulp-notify'),
+    runSequence = require('run-sequence'),
+    cssnano = require('gulp-cssnano'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('browserSync', function() {
   browserSync({
@@ -28,8 +37,57 @@ gulp.task('less', function() {
     .pipe(browserSync.reload({stream: true}));
 });
 
+// START BUILD
+gulp.task('clean', function(){
+	return del.sync('dist');
+});
+
+gulp.task('copyHtml', function() {
+	return gulp.src('app/index.html')
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('copyFonts', function() {
+	return gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('copyCompressImg', function() {
+	return gulp.src('app/img/**/*.{png,jpg,gif,svg}')
+		//.pipe(imagemin({
+		//	imagemin.optipng({optimizationLevel: 3}),
+		//	imagemin.jpegtran({progressive: true})
+		//}))
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('copyMinCss', function() {
+	return gulp.src('app/css/style.css')
+		.pipe(cssnano())
+		//.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('copyLibsJs', function() {
+	gulp.src('app/libs/bower/jquery/dist/jquery.min.js')
+		.pipe(gulp.dest('dist/libs/bower/jquery/dist'))
+    gulp.src('app/libs/bower/wow/dist/wow.min.js')
+		.pipe(gulp.dest('dist/libs/bower/wow/dist'));
+});
+
+gulp.task('copyJs', function() {
+	return gulp.src('app/js/**/*')
+		.pipe(gulp.dest('dist/js'));
+});
+// END BUILD
+
 // main point
-gulp.task('default', ['browserSync', 'less'], function () {
+gulp.task('build', function(callback) {
+	runSequence('clean', 'copyHtml', 'copyFonts', 'copyCompressImg', 'copyMinCss', 'copyLibsJs', 'copyJs', callback);
+});
+
+gulp.task('default', ['browserSync', 'less'], function() {
 
   // обновляем браузер при любых изменениях в html, less или js
   gulp.watch('app/less/**/*.less', ['less']);
