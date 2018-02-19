e5f4a4e3e8de487e2202ade8af38c80457e99322
vasily-d <den771981@mail.ru>
2018-02-19 15:06:15 +0300

    fi

--- 16 wireless bluetooth speaker/js/index-page.js
+++ 16 wireless bluetooth speaker/js/index-page.js
@@ -5,7 +5,29 @@ $(function() {
 	  type: 'image'
 	  // other options
 	});
-	
+
+	// popup
+	// ставим на кнопку которая вызывает данный popup
+	$('.js-popup').on('click', function(e) {
+	  e.preventDefault();
+
+	  // поиск popup и popup-overlay для их открытия
+	  // $('.popup').css('display', 'block');
+	  // $('.popup-overlay').css('display', 'block');
+	  // or
+	  $('.popup').slideDown();
+	  $('.popup-overlay').css('display', 'block');
+	  
+	  // поиск button close popup
+	  $('#js-close-popup, .popup-overlay').on('click', function(e) {
+	    // $('.popup').css('display', 'none');
+	    // $('.popup-overlay').css('display', 'none');
+	    // or
+	    $('.popup').slideUp();
+	    $('.popup-overlay').css('display', 'none');
+	  });
+	});
+
 	
 
 });
