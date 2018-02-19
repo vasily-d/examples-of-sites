c1aff5701360c94bdbeeb599008c0eeb9050afbf
vasily-d <den771981@mail.ru>
2018-02-19 11:35:15 +0300

    add new markup

---
+++ 16 wireless bluetooth speaker/js/index-page.js
@@ -0,0 +1,11 @@
+$(function() {
+	// magnific-popup
+	$('.image-link').magnificPopup({type:'image'});
+	$('.popup-link1').magnificPopup({
+	  type: 'image'
+	  // other options
+	});
+	
+	
+
+});
--- 16 wireless bluetooth speaker/readme.txt
+++
@@ -1,2 +0,0 @@
-some of my sites without the source code of the layout - at the request of the customer
-некоторые мои сайты без исходника кода верстки - по просьбе заказчика
